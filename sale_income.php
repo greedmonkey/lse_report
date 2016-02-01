4a55caab3b18d8c55163d2cd0276d9c2adb351c8
Kreang <kreangkrai.sup@gmail.com>
2016-02-01 23:07:50 +0700

    finish course name

--- sale_income.php
+++ sale_income.php
@@ -1,5 +1,46 @@
 <?php 
     require_once('../data/kreang_connection.php');
+    ob_start();
+    header("content-type:application/csv;charset=UTF-8");
+    header("Content-Disposition:attachment;filename=contacts.csv");
+
+    $list = array
+    (
+    "เกรียงไกร,ลิ้นกับฟัน,,Norway",
+    "Glenn,Quagmire,Oslo,Norway",
+    );
+
+    $fileName2 = 'contacts.csv';
+    $file = fopen("contacts.csv","w");
+
+    foreach ($list as $line)
+      {
+      fputcsv($file,explode(',',$line));
+      }
+
+    fclose($file);
+    echo "<a href=$fileName2>Download</a>";
+
+
+    $filName = "customer2.csv";
+    $objWrite = fopen("customer2.csv", "w");
+    fwrite($objWrite, "\"C001\",เกรียงไกร ลิ้นกับฟัน\",\"win.weerachai@thaicreate.com\",\"TH\",\"1000000\",\"600000\" \n");
+    fwrite($objWrite, "\"C002\",\"John  Smith\",\"john.smith@thaicreate.com\",\"EN\",\"2000000\",\"800000\" \n");
+    fwrite($objWrite, "\"C003\",\"Jame Born\",\"jame.born@thaicreate.com\",\"US\",\"3000000\",\"600000\" \n");
+    fwrite($objWrite, "\"C004\",\"Chalee Angel\",\"chalee.angel@thaicreate.com\",\"US\",\"4000000\",\"100000\" \n");
+    fclose($objWrite);
+    echo "<br>Generate CSV Done.<br><a href=$filName>Download</a>";
+    ob_end_flush();
+
+
+
+
+
+
+
+
+
+
     if ( $conn = Connection::databaseConnect() ) {
         $select_sql = "SELECT cl.ID, CL.FIRST_NAME_EN FROM client AS cl WHERE FIRST_NAME_EN  LIKE ?";
         if ($stmt = $conn -> prepare($select_sql)) {
--- testSelect.php
+++ testSelect.php
@@ -1,16 +1,16 @@
 <?php 
     require_once('../data/kreang_connection.php');
     if ( $conn = Connection::databaseConnect() ) {
-        $select_sql = "SELECT cl.ID, CL.FIRST_NAME_EN FROM client AS cl WHERE FIRST_NAME_EN  LIKE ?";
+        $select_sql = "SELECT cpa.PAYMENT_DATE, cpa.RECEIPT_NO, c.CLIENT_NO, c.FIRST_NAME_EN, c.MIDDLE_NAME_EN, c.LAST_NAME_EN, gn.NAME as `group_name`, rct.NAME_EN as `course type`, cos.NAME as `course name` FROM client_payment_amount as cpa INNER JOIN client AS c ON  cpa.CLIENT_ID = c.ID INNER JOIN client_payment as cp ON cpa.CLIENT_PAYMENT_ID = cp.ID INNER JOIN group_name as gn ON cp.GROUP_ID = gn.ID INNER JOIN ref_course_type as rct ON rct.ID = gn.COURSE_TYPE_ID INNER JOIN course as cos ON cos.ID = gn.COURSE_ID";
         if ($stmt = $conn -> prepare($select_sql)) {
              $first_name_en = 'Achiraya';
-            if($stmt->bind_param('s', $first_name_en)){
+//            if($stmt->bind_param('s', $first_name_en)){
+            if(true){
                 if($stmt->execute()){
                     $result = $stmt->get_result();
                     while ($row = $result->fetch_array(MYSQLI_NUM)) {
-                        echo 'ID = '.$row[0];
+                        print_r($row);
                         echo '<br>';
-                        echo 'first_name_en = '.$row[1];
                     }
                 }else {
                     echo $error_database_statement_execute . ' (errcode: ' . $stmt->errno . ', detail: ' . $stmt->error . ')';
